e926c22fe8e0096549d228767e7bb408f51fee65
Ninad Sutrave <ninadsutrave@gmail.com>
2022-11-18 01:16:29 +0530

    Update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Navbar = () => {
   return (
     <div className="Footer">
-        Designed By: <a href="https://dribbble.com/Sanyukta_Andi" target="_blank" rel="noreferrer">Sanyukta Marandi</a>
+        MIT © <a href="https://github.com/NinadSutrave" target="_blank" rel="noreferrer">Ninad Sutrave</a>
     </div>
   )
 }
